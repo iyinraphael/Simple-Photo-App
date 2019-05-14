6af5f15d7e21a9b0b32480e724e7a1bf4c228fe6
iyinraphael <iyinraphael@gmail.com>
2019-05-14 09:02:58 -0700

    custom imageView in detailView controller

--- Simple-Photo-App/View Controller/PhotoDetailViewController.swift
+++ Simple-Photo-App/View Controller/PhotoDetailViewController.swift
@@ -12,9 +12,23 @@ class PhotoDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        
+        createCustomImageView()
     }
     
+    //Creating custom imageView contrainst
+    private func createCustomImageView() {
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        let imageView = UIImageView(frame: CGRect(x: 100, y: 100, width: 150, height: 150))
+        imageView.clipsToBounds = true
+        
+        let radius = imageView.frame.width / 0.5
+        imageView.layer.cornerRadius = radius
+        imageView.layer.borderWidth = 1.0
+        imageView.layer.borderColor = UIColor.black.cgColor
+        
+        
+    }
 
 }
