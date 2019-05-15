bea666e587efe2bab24098ad04a4d517f9a2ca6a
iyinraphael <iyinraphael@gmail.com>
2019-05-14 22:59:12 -0700

    implementation of cache and concurrency, app works smooth

--- Simple-Photo-App/View Controller/PhotoViewController.swift
+++ Simple-Photo-App/View Controller/PhotoViewController.swift
@@ -13,7 +13,9 @@ class PhotoViewController: UIViewController {
 
     let reuseIdentifier = "ThumbnailPhotoCollectionViewCell"
     let photoController = PhotoController()
-    
+    private let cache = Cache<Int64, UIImage>()
+    private var operations = [Int64 : Operation]()
+    private let photoFetchQueue = OperationQueue()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,26 +36,40 @@ class PhotoViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
        
     }
-
-}
-
-    // MARK: - UICollection Delagate and Data Source
-extension PhotoViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return photoController.photos.count
-    }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? ThumbnailPhotoCollectionViewCell else {return UICollectionViewCell()}
+    //MARK: - Cache and Concurrency(NSOperation)
+    private func loadImage(forCell cell: ThumbnailPhotoCollectionViewCell, forItemAt indexPath: IndexPath) {
         let photo = photoController.photos[indexPath.item]
+        if let cachedImage = cache.value(for: photo.id as Int64) {
+            cell.imageView.image = cachedImage
+            return
+        }
+        let fetchOp = FetchPhotoOperation(photo: photo)
+        let cacheOp = BlockOperation {
+            if let image = fetchOp.image {
+                self.cache.cache(value: image, for: photo.id)
+            }
+        }
+        let completionOp = BlockOperation {
+            defer {self.operations.removeValue(forKey:photo.id)}
+            
+            if let currentIndexpath = self.collectionVIew.indexPath(for: cell),
+                currentIndexpath != indexPath {
+                return
+            }
+            if let image = fetchOp.image {
+                cell.imageView.image = image
+            }
+        }
+        cacheOp.addDependency(fetchOp)
+        completionOp.addDependency(fetchOp)
         
-        let imageData = stringToData(str: photo.thumbnailURL)
-        cell.imageView.image = UIImage(data: imageData!)
-        return cell
+        photoFetchQueue.addOperation(fetchOp)
+        photoFetchQueue.addOperation(cacheOp)
+        OperationQueue.main.addOperation(completionOp)
+        
+        operations[photo.id] = fetchOp
     }
-    
-    
-    
     
     private func stringToData(str: String?) -> Data? {
         var newData:Data?
@@ -67,8 +83,22 @@ extension PhotoViewController: UICollectionViewDelegate, UICollectionViewDataSou
         }
         return newData
     }
+
 }
 
+    // MARK: - UICollection Delagate and Data Source
+extension PhotoViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photoController.photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? ThumbnailPhotoCollectionViewCell else {return UICollectionViewCell()}
+        
+        loadImage(forCell: cell, forItemAt: indexPath)
+        
+        return cell
+    }
+}
 
-// MARk: - NSFetchControllerDelegate
 
