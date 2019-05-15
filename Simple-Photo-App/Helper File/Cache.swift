817bf2a58e932d016ad513b4c18c0df97e0443f3
iyinraphael <iyinraphael@gmail.com>
2019-05-14 22:27:22 -0700

    created cache helper class

---
+++ Simple-Photo-App/Helper File/Cache.swift
@@ -0,0 +1,31 @@
+//
+//  Cache.swift
+//  Simple-Photo-App
+//
+//  Created by Iyin Raphael on 5/14/19.
+//  Copyright © 2019 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class Cache<Key: Hashable, Value> {
+    
+    func cache(value: Value, for key: Key) {
+        queue.async {
+            self.cache[key] = value
+        }
+    }
+    
+    func value(for key: Key) -> Value? {
+        return queue.sync { cache[key] }
+    }
+    
+    func clear() {
+        queue.async {
+            self.cache.removeAll()
+        }
+    }
+    
+    private var cache = [Key : Value]()
+    private let queue = DispatchQueue(label: "com.IyinRaphael.Simple_Photo_App.CacheQueue")
+}
